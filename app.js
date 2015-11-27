741c88b3aa6e9cc512685f9cadfa7c2e84c49999
jpaulini <jpaulini@gmail.com>
2015-11-27 10:45:20 -0300

    removed form from layout, added to an index section

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+var hbs = require('hbs');
+
+hbs.registerHelper('section', function(name, options) {
+   if(!this._sections) this._sections= {};
+   this._sections[name] = options.fn(this);
+   return null;
+});
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
